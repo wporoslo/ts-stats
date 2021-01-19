b6aaf8febb2b3ef5bd4dcea0030bf549238c8548
wporoslo <wporoslo@gmail.com>
2021-01-19 14:38:06 +0100

    move code a little for better readibility

--- build/index.js
+++ build/index.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
 var Summary_1 = require("./Summary");
 var matchReader = MatchReader_1.MatchReader.fromCsv('football.csv');
-matchReader.load();
 var summary = Summary_1.Summary.winsAnalysisAndHtmlReport('Liverpool');
+matchReader.load();
 summary.buildAndPrintReport(matchReader.matches);
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,7 @@ import {MatchReader} from './MatchReader'
 import {Summary} from './Summary'
 
 const matchReader = MatchReader.fromCsv('football.csv')
-matchReader.load()
-
 const summary = Summary.winsAnalysisAndHtmlReport('Liverpool')
 
+matchReader.load()
 summary.buildAndPrintReport(matchReader.matches)
